114a10e846257c8f69b9d4a05640a46ed0b9528c
Issah <issahissein10@gmail.com>
2026-09-17 19:11:15 +0200

    add_sidebar
    :wq

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ Tea,3
 
 beverages = pd.read_csv(io.StringIO(csv))
 
+
 csv2 = '''
 food_item,food_price
 cookie juice,2.5
@@ -23,31 +24,49 @@ muffin,3
 food_items = pd.read_csv(io.StringIO(csv2))
 
 
-answer = """
+answer = '''
 SELECT * FROM beverages
 CROSS JOIN food_items
-"""
+'''
 
 solution = duckdb.sql(answer).df()
 
 
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review?",
+        ("Joins", "GroupBy", "Windows Functions"),
+        index=None,
+        placeholder="Select a theme...",
+    )
+
+    st.write("You selected:", option)
+
+
 st.header("enter your code:")
-query = st.text_area(label="votre code SQL ici:", key="user_input")
+query = st.text_area(
+    label="votre code SQL ici:",
+    key="user_input"
+)
 
 if query:
     result = duckdb.sql(query).df()
     st.dataframe(result)
 
 
-tab2, tab3 = st.tabs(["Tables", "Solution"])
+tab2, tab3 = st.tabs(("Tables", "Solution"))
+
 
 with tab2:
     st.write("table: beverages")
     st.dataframe(beverages)
+
     st.write("table: food_items")
     st.dataframe(food_items)
+
     st.write("expected:")
     st.dataframe(solution)
+
 
 with tab3:
     st.write(answer)
